5811272512ef473da12f24e0cd2fa992c6dc48fa
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-02-08 17:24:17 +0300

    Add dynamically created data using JS

--- README.md
+++ README.md
@@ -1,1 +1,126 @@
-# Concert-Capstone-Project
+<a name="readme-top"></a>
+
+<div align="center">
+
+  <br/>
+
+# 📗 Table of Contents
+
+- [📖 About the Project](#about-project)
+  - [🛠 Built With](#built-with)
+    - [Tech Stack](#tech-stack)
+    - [Key Features](#key-features)
+  - [🚀 Live Demo](#live-demo)
+- [💻 Getting Started](#getting-started)
+  - [Setup](#setup)
+  - [Prerequisites](#prerequisites)
+  - [Install](#install)
+  - [Usage](#usage)
+  - [Run tests](#run-tests)
+  - [Deployment](#triangular_flag_on_post-deployment)
+- [👥 Authors](#authors)
+- [🔭 Future Features](#future-features)
+- [🤝 Contributing](#contributing)
+- [⭐️ Show your support](#support)
+- [🙏 Acknowledgements](#acknowledgements)
+- [❓ FAQ (OPTIONAL)](#faq)
+- [📝 License](#license)
+
+<!-- PROJECT DESCRIPTION -->
+
+# 📖 CARIBBEAN CONCERT <a name="about-project"></a>
+
+<!-- > Describe your project in 1 or 2 sentences. -->
+
+**Caribbean Concert** is a project built purely on CSS, HTML, and Javascript for showing an upcoming concert wold tour in the caribbean. The website is an appreciation from Behance by Cindy Shin.
+
+## 🛠 Built With <a name="built-with"></a>
+
+- **HTML**
+- **CSS**
+- **JAVASCRIPT**
+
+<!-- Features -->
+
+### Key Features <a name="key-features"></a>
+
+<!-- > Describe between 1-3 key features of the application. -->
+
+- **Responsive Mobile Menu**
+- **Linked html pages**
+- **Dynamic content**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- LIVE DEMO -->
+
+## 🚀 Live Demo <a name="live-demo"></a>
+
+<!-- > Add a link to your deployed project. -->
+
+- [Live Demo Link](https://adholah96.github.io/climate-summit/)
+- [Video Link](https://www.loom.com/share/091dfd67fd6f422ea5949891d2340f0a)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- AUTHORS -->
+
+## 👥 Authors <a name="authors"></a>
+
+<!-- > Mention all of the collaborators of this project. -->
+
+👤 **KOSHAO KAZUNGU GODFREY**
+
+- GitHub: [@adholah96](https://github.com/Koshao-sys)
+- Twitter: [@nerdy*me*](https://twitter.com/KoshaoKG)
+- LinkedIn: [heldricks-arthur](https://linkedin.com/in/godfrey-kazungu-b8b81b245)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- FUTURE FEATURES --> 
+
+## 🔭 Future Features <a name="future-features"></a>
+
+ <!-- Describe 1 - 3 features you will add to the project. --> 
+
+- **Concert Videos**
+- **Hall of Fame**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- CONTRIBUTING -->
+
+## 🤝 Contributing <a name="contributing"></a>
+
+Contributions, issues, and feature requests are welcome!
+
+<!-- Feel free to check the [issues page](../../issues/). -->
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- SUPPORT -->
+
+## ⭐️ Show your support <a name="support"></a>
+
+Always leave a ⭐️ if you like this project and any of my other projects.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- ACKNOWLEDGEMENTS -->
+
+## 🙏 Acknowledgments <a name="acknowledgements"></a>
+
+<!-- > Give credit to everyone who inspired your codebase. -->
+
+I would like to thank Cindy Shin from Behance for coming up with such a great a design template.
+I would like to thank my coding partners and standup team for the great advice towrds completion of this project.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- LICENSE -->
+
+## 📝 License <a name="license"></a>
+
+This project is [MIT](./MIT.md) licensed.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+const toggleNav = document.querySelector('.hamburger');
+const nav = document.querySelector('.menu');
+const closeMenu = document.querySelector('.close-menu');
+const closeLinks = document.querySelectorAll('.menu a');
+const screenSize = window.matchMedia('(width > 768px)');
+const moreInfo = document.querySelector('.more-div');
+const partnerContainer = document.querySelector('.partner-container');
+const footerContainer = document.querySelector('.footer-container');
+
+toggleNav.addEventListener('click', () => {
+  nav.classList.toggle('open');
+  closeMenu.style.display = nav.classList.contains('open') ? 'block' : 'none';
+  toggleNav.style.display = nav.classList.contains('open') ? 'none' : 'block';
+});
+
+closeMenu.addEventListener('click', () => {
+  nav.classList.remove('open');
+  closeMenu.style.display = 'none';
+  toggleNav.style.display = 'block';
+});
+
+closeLinks.forEach((closeLink) => {
+  closeLink.addEventListener('click', (e) => {
+    nav.classList.remove('open');
+    closeMenu.style.display = 'none';
+    screenSize.addEventListener('change', () => {
+      screenSize.matches = true ? toggleNav.style.display = 'none' : toggleNav.style.display = 'block';
+    });
+    e.preventDefault();
+    const targetId = e.target.getAttribute('href');
+    const target = document.querySelector(targetId);
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
 const speakerObj = {
   speakerHeader: 'Featured Guests',
   canvas: './images/chess.svg',
@@ -55,40 +90,58 @@ const speakers = document.createElement('div');
 
 for (let i = 0; i < speakerObj.speakerDetails.length; i += 1) {
   speakers.setAttribute('class', 'speakers');
+  const chessBg = document.createElement('div');
+  chessBg.setAttribute('class', 'chess-Bg');
+
   const speaker = document.createElement('div');
   speaker.setAttribute('class', 'speaker');
+
   const imgHolder = document.createElement('div');
   imgHolder.setAttribute('class', 'img-Holder');
+
   const speakerImg = document.createElement('img');
   speakerImg.setAttribute('class', 'speaker-Img');
   speakerImg.setAttribute('src', speakerObj.speakerDetails[i].gif);
-  /* const speakerImgcanvas = document.createElement('img');
-  speakerImgcanvas.setAttribute('class', 'speaker-Imgcanvas');
-  speakerImgcanvas.setAttribute('src', speakerObj.canvas) */
+
   const descBox = document.createElement('div');
   descBox.setAttribute('class', 'desc-box');
+
   const descBoxhead = document.createElement('h2');
   descBoxhead.setAttribute('class', 'desc-Boxhead');
   descBoxhead.textContent = speakerObj.speakerDetails[i].name;
+
   const descBoxheadpara = document.createElement('p');
   descBoxheadpara.setAttribute('class', 'desc-Boxheadpara');
   descBoxheadpara.textContent = speakerObj.speakerDetails[i].bio;
+
   const descBoxspan = document.createElement('span');
   descBoxspan.setAttribute('class', 'desc-Boxspan');
+
   const descBoxpara = document.createElement('p');
   descBoxpara.setAttribute('class', 'desc-Boxpara');
   descBoxpara.textContent = speakerObj.speakerDetails[i].desc;
-  /* imgHolder.appendChild(speakerImgcanvas); */
+
   imgHolder.appendChild(speakerImg);
   descBox.appendChild(descBoxhead);
   descBox.appendChild(descBoxheadpara);
   descBox.appendChild(descBoxspan);
   descBox.appendChild(descBoxpara);
-
   speaker.appendChild(imgHolder);
   speaker.appendChild(descBox);
-  speakers.appendChild(speaker);
+  chessBg.appendChild(speaker);
+  speakers.appendChild(chessBg);
 }
 
 speakerSection.appendChild(header);
 speakerSection.appendChild(speakers);
+
+const childElements = document.querySelectorAll('.chess-Bg');
+
+moreInfo.addEventListener('click', () => {
+  for (let x = 2; x < childElements.length; x += 1) {
+    childElements[x].style.display = 'block';
+  }
+  moreInfo.style.display = 'none';
+  partnerContainer.style.display = 'block';
+  footerContainer.style.display = 'block';
+});
